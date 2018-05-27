c78aefd035d82a51a60e4aca32d1cff86c3b2743
Juan-Pablo Barahona <juanpablo.barahonaa@gmail.com>
2018-05-27 01:36:41 -0400

    init bot phr

--- app.py
+++ app.py
@@ -1,9 +1,21 @@
-from flask import Flask
+#!/usr/bin/env python3
+"""Example bot that returns a synchronous response."""
+
+from flask import Flask, request, json
+
 app = Flask(__name__)
- 
-@app.route("/")
-def hello():
-    return "Hello World!"
- 
-if __name__ == "__main__":
-    app.run()
+
+@app.route('/', methods=['POST'])
+def on_event():
+  """Handles an event from Hangouts Chat."""
+  event = request.get_json()
+  if event['type'] == 'ADDED_TO_SPACE' and event['space']['type'] == 'ROOM':
+    text = 'Thanks for adding me to "%s"!' % event['space']['displayName']
+  elif event['type'] == 'MESSAGE':
+    text = 'You said: `%s`' % event['message']['text']
+  else:
+    return
+  return json.jsonify({'text': text})
+
+if __name__ == '__main__':
+  app.run()
