86d35b2e304e5131b31b483c8178e15d18818c85
Juan-Pablo Barahona <juanpablo.barahonaa@gmail.com>
2018-05-28 14:26:10 -0400

    servicio buscar credenciales sfsf

--- app.py
+++ app.py
@@ -3,64 +3,7 @@
 
 from flask import Flask, request, json
 import src.mongo.hangouts as hangouts
-
-cliente = 'd'
-ambiente = 'prd'
-link = 'https://google.com'
-usuario = 'dsadas'
-password = 'dsadsda'
-companyId = 'C000000'
-
-i = 0
-a = []
-
-while (i < 2):
-	a.append({
-				'header': {
-			        'title': 'Arauco',
-			        'subtitle': cliente
-			      },
-			      'sections': [
-			        {
-			          'widgets': [
-			          		{
-			          		'buttons': [
-				                	{
-					                  'textButton': {
-					                    'text': 'Link '+ ambiente,
-					                    'onClick': {
-					                      'openLink': {
-					                        'url': link
-					                      }
-					                    }
-					                  }
-					                }
-				              	]
-							},
-							{
-								'keyValue': {
-								  'topLabel': 'Usuario',
-								  'content': usuario
-								}
-							},
-							{
-								'keyValue': {
-								  'topLabel': 'Contrasena',
-								  'content': password
-								}
-							},
-							{
-								'keyValue': {
-								  'topLabel': 'Company ID',
-								  'content': companyId
-								}
-							}
-			          ]
-			        }
-			      ]
-			    }
-			)
-	i = i + 1
+import src.service.buscarCredencialesSFSF as bc
 
 app = Flask(__name__)
 
@@ -74,14 +17,10 @@ def on_event():
   if event['type'] == 'ADDED_TO_SPACE' and event['space']['type'] == 'ROOM':
     text = 'Thanks for adding me to "%s"!' % event['space']['displayName']
   elif event['type'] == 'MESSAGE':
-    text = 'You said: `%s`' % event['message']['text']
+    text = event['message']['text']
   else:
     return
-  return json.jsonify(
-					{
-					  'cards': a
-					}
-  				)
+  return json.jsonify(bc.BuscarCredenciales(text))
 
 if __name__ == '__main__':
   app.run()
---
+++ src/service/buscarCredencialesSFSF.py
@@ -0,0 +1,92 @@
+import json
+from oauth2client.service_account import ServiceAccountCredentials
+import gspread
+import pandas
+
+config_file = open('./src/service/config.json','r')
+config = json.load(config_file)
+
+scope = config['scope']
+creds = ServiceAccountCredentials.from_json_keyfile_name(so.environ['CLIENT_SECRET_TEST'],scope)
+client = gspread.authorize(creds)
+
+sheet = client.open(config['archivo']['credencialesSFSF']['sheet'])
+worksheet = sheet.worksheet(config['archivo']['credencialesSFSF']['workSheet'])
+
+wsheet = worksheet.get_all_values()
+
+# header -> primera fila es un comentario
+df = pandas.DataFrame.from_records(wsheet[1:])
+
+# .values -> array o matriz
+header = (df.loc[df[0] == 'Cliente']).values[0]
+dataSet = (df.loc[df[0] != 'Cliente']).values
+
+dff = pandas.DataFrame.from_records(dataSet, columns = header)
+
+# out json
+def _outCardsCredencialesSFSF(payload):
+    cards = []
+    result = payload
+
+    for index, row in result.iterrows():
+        cards.append(
+            {
+                    'header': {
+                        'title': 'Arauco',
+                        'subtitle': row.Cliente
+                      },
+                      'sections': [
+                        {
+                          'widgets': [
+                                {
+                                'buttons': [
+                                        {
+                                          'textButton': {
+                                            'text': 'Link '+ row['Tipo Acceso'],
+                                            'onClick': {
+                                              'openLink': {
+                                                'url': row.Link
+                                              }
+                                            }
+                                          }
+                                        }
+                                    ]
+                                },
+                                {
+                                    'keyValue': {
+                                      'topLabel': 'Usuario',
+                                      'content': row.Usuario
+                                    }
+                                },
+                                {
+                                    'keyValue': {
+                                      'topLabel': 'Contraseña',
+                                      'content': row.Contraseña
+                                    }
+                                },
+                                {
+                                    'keyValue': {
+                                      'topLabel': 'Company ID',
+                                      'content': row['Company ID']
+                                    }
+                                }
+                          ]
+                        }
+                      ]
+                    }
+        )
+        
+    return {'cards':cards}
+
+def BuscarCredenciales (cliente, ambiente = ''):
+	cliente = dff.Cliente == cliente
+
+	config.close()
+
+	if (ambiente != ''):
+		ambiente = dff['Tipo Acceso'] == ambiente
+
+		return _outCardsCredencialesSFSF(dff.loc[cliente & ambiente])
+	else:
+		return _outCardsCredencialesSFSF(dff.loc[cliente])
