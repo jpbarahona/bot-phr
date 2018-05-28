5c695ad7f6df49bf6e3700d7fce9220d17c5e8c1
Juan-Pablo Barahona <juanpablo.barahonaa@gmail.com>
2018-05-28 01:40:03 -0400

    test card

--- app.py
+++ app.py
@@ -7,10 +7,11 @@ import src.mongo.hangouts as hangouts
 app = Flask(__name__)
 
 @app.route('/', methods=['POST'])
+
 def on_event():
   event = request.get_json()
   
-  hangouts.guardarMensaje(event)
+  #hangouts.guardarMensaje(event)
 
   if event['type'] == 'ADDED_TO_SPACE' and event['space']['type'] == 'ROOM':
     text = 'Thanks for adding me to "%s"!' % event['space']['displayName']
@@ -18,7 +19,60 @@ def on_event():
     text = 'You said: `%s`' % event['message']['text']
   else:
     return
-  return json.jsonify({'text': text})
+  return json.jsonify(
+					  	{
+					  'cards': [
+					    {
+					      'header': {
+					        'title': 'Arauco',
+					        'subtitle': 'Credenciales'
+					      },
+					      'sections': [
+					        {
+					          'widgets': [
+					              {
+					                'keyValue': {
+					                  'topLabel': 'Ambiente',
+					                  'content': 'PRD'
+					                  }
+					              },
+					              {
+					                'keyValue': {
+					                  'topLabel': 'Usuario',
+					                  'content': 'phr'
+					                }
+					              },
+					              {
+					                'keyValue': {
+					                  'topLabel': 'Contraseña',
+					                  'content': 'dsa'
+					                }
+					              },
+					              {
+					                'keyValue': {
+					                  'topLabel': 'Company ID',
+					                  'content': 'C000000000'
+					                }
+					              },
+					             	'buttons': [
+									{
+					                  'textButton': {
+					                      'text': 'Link',
+					                      'onClick': {
+						                      'openLink': {
+						                        'url': 'https://google.com/'
+						                      }
+					                      }
+					                  }
+					                }
+								]
+					          ]
+					        }
+					      ]
+					    }
+					  ]
+					}
+  	)
 
 if __name__ == '__main__':
   app.run()
